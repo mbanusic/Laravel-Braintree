b08635b25861f785c73106f574c6d33db2730763
Dan Hunsaker <danhunsaker@gmail.com>
2019-03-19 22:40:59 -0600

    Fix calling logic to actually check for `get*`

--- src/Braintree.php
+++ src/Braintree.php
@@ -19,17 +19,18 @@ class Braintree
 
     public function __call(string $method, array $arguments)
     {
-        // Remove the get*-prefix to get a class name
-        $sdkClass = substr($method, 3);
+        if (substr($method, 0, 3) == 'get') {
+            // Remove the get*-prefix to get a class name
+            $sdkClass = substr($method, 3);
 
-        // A class has been previously called
-        if ($this->braintreeClass) {
-            return forward_static_call_array([$this->braintreeClass, $method], $arguments);
+            // If method is get*, we will store the last called class
+            if (class_exists($apiClass = "Braintree\\$sdkClass")) {
+                $this->braintreeClass = $apiClass;
+            }
         }
-
-        // Method is get* so we will store the last called class
-        if (class_exists($apiClass = "Braintree\\$sdkClass")) {
-            $this->braintreeClass = $apiClass;
+        // A class has been previously called
+        elseif ($this->braintreeClass) {
+            return forward_static_call_array([$this->braintreeClass, $method], $arguments);
         }
 
         return $this;
